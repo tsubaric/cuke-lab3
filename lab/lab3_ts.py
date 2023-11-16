737ab5b269641677a2b6cafa35d1893b72127b7b
tsubaric <tommysubaric@gmail.com>
2023-11-15 23:38:18 -0600

    go

--- lab/lab3_ts.py
+++ lab/lab3_ts.py
@@ -9,13 +9,11 @@ import codegen.cpu
 from core.ir import *
 from core.ast import *
 
-
 def get_obj(ir: (Index, Scalar)):
     obj = ir
     while hasattr(obj, 'dobject'):
         obj = obj.dobject
     return obj
-
 
 def replace_index_with_scalar(ir, old, new):
     if type(ir) == list or type(ir) == tuple:
@@ -52,18 +50,18 @@ def replace_index_with_scalar(ir, old, new):
         else:
             replace_index_with_scalar(ir.rhs, old, new)
 
-#Define it by yourself
 def rebind_iterate(ir, old_idx, new_idx):
-    if type(ir) == list or type(ir) == tuple:
+    if isinstance(ir, (list, tuple)):
         for l in ir:
             rebind_iterate(l, old_idx, new_idx)
-    elif type(ir) == Assignment:
-        rebind_iterate(ir.left,  old_idx, new_idx)
-        rebind_iterate(ir.right, old_idx, new_idx)
-    elif type(ir) == Index:
+    elif isinstance(ir, Assignment):
+        rebind_iterate(ir.lhs, old_idx, new_idx)
+        rebind_iterate(ir.rhs, old_idx, new_idx)
+    elif isinstance(ir, Index):
         if ir.index == old_idx:
-            ir.index = new_idx:
-    # Some other types 
+            ir.index = new_idx
+    elif isinstance(ir, Loop):
+        rebind_iterate(ir.body, old_idx, new_idx)
 
 def fusable_level(node0, node1):
     def _fusable_level(loop0, loop1, level):
@@ -99,7 +97,6 @@ def get_rhs(node):
     return _get_rhs(node.compute[0])
 
 def fuse(ast_wit_ir):
-	#ast_wit_irt =  #implement here
     elementwise_op = op_mapping
     node = ast_wit_ir
     def action(node, res):
@@ -107,74 +104,73 @@ def fuse(ast_wit_ir):
             if type(node.operators[0]) == TensorOp and node.operators[0].op_type in elementwise_op:      
                 print("Find fusable pairs! Left")
                 #Do something here
+                x = 0
             if type(node.operators[1]) == TensorOp and node.operators[1].op_type in elementwise_op:
                 print("Find fusable pairs! Right")
                 #Do something here
+                x = 0
             if type(node.operators[0]) == TensorOp and node.operators[0].op_type == 'einsum':
                 #Do something here
+                x = 0
             if type(node.operators[1]) == TensorOp and node.operators[1].op_type == 'einsum':
                 #Do something here
+                x = 0
            
     t = helpers.Traversal(action)
     t(node)
     return node
 
-
 def test1():
     A = Tensor('a', (30, 30))
     B = Tensor('b', (30, 30))
     C = Tensor('c', (30, 30))
-    D = Tensor('d', (30, 30))
-    E = Tensor('e', (30, 30))
 
     res1 = A + B 
-    res2 = C + D
-    res = res1 + res2 + E
-    res_with_ir = gen_ir(res)
-    code = codegen.cpu.gen_cpp(res_with_ir)
-    print(code)
-    new_res_with_ir = fuse(res_with_ir)
-
-def test2():
-    A = Tensor('a', (20, 30))
-    B = Tensor('b', (30, 40))
-    C = Tensor('c', (20, 40))
-    D = Tensor('d', (40, 40))
-
-    res1 = A @ B 
-    res2 = C @ D
+    res2 = + C
     res = res1 + res2
     res_with_ir = gen_ir(res)
     code = codegen.cpu.gen_cpp(res_with_ir)
     print(code)
-    new_res_with_ir = fuse(res_with_ir)
+    # new_res_with_ir = fuse(res_with_ir)
+    # print(new_res_with_ir)
+
+def test2():
+    A = Tensor('a', (10, 20))
+    B = Tensor('b', (20, 30))
+    C = Tensor('c', (10, 30))
+
+    res1 = A @ B 
+    res2 = + C
+    res = res1 + res2
+    res_with_ir = gen_ir(res)
+    code = codegen.cpu.gen_cpp(res_with_ir)
+    print(code)
+    # new_res_with_ir = fuse(res_with_ir)
+    # print(new_res_with_ir)
+
+def test3():
+    A = Tensor('a', (30, 30))
+    B = Tensor('b', (30, 30))
+    C = Tensor('c', (30, 30))
+
+    res = A + B + C
+    res_with_ir = gen_ir(res)
+    code = codegen.cpu.gen_cpp(res_with_ir)
+    print(code)
+    # new_res_with_ir = fuse(res_with_ir)
+    # print(new_res_with_ir)
+
+def test4():
+    A = Tensor('a', (10, 20))
+    B = Tensor('b', (20, 30))
+    C = Tensor('c', (10, 30))
+
+    res = A @ B + C
+    res_with_ir = gen_ir(res)
+    code = codegen.cpu.gen_cpp(res_with_ir)
+    print(code)
+    # new_res_with_ir = fuse(res_with_ir)
+    # print(new_res_with_ir)
 
 if __name__ == "__main__":
-    test1()
-
-
-#     torch::Tensor add_add_a_b_c(torch::Tensor obj_a, torch::Tensor obj_b, torch::Tensor obj_c)
-# {
-    # auto a = obj_a.accessor<float, 2>();
-    # auto b = obj_b.accessor<float, 2>();
-    # torch::Tensor obj_arr2 = torch::empty({30,30}, at::kFloat);
-    # auto arr2 = obj_arr2.accessor<float, 2>();
-    # for (int _l0 = 0; _l0 < 30; _l0 += 1) {
-    #   for (int _l1 = 0; _l1 < 30; _l1 += 1) {
-    #       arr2[_l0][_l1] = a[_l0][_l1] + b[_l0][_l1];
-    # } 
-    # } 
-    # auto c = obj_c.accessor<float, 2>();
-    # torch::Tensor obj_arr6 = torch::empty({30,30}, at::kFloat);
-    # auto arr6 = obj_arr6.accessor<float, 2>();
-    # for (int _l2 = 0; _l2 < 30; _l2 += 1) {
-    #   for (int _l3 = 0; _l3 < 30; _l3 += 1) {
-    #       arr6[_l2][_l3] = arr2[_l2][_l3] + c[_l2][_l3];
-    # } 
-    # } 
-    # return obj_arr6;
-
-    # }
-
-    # code = codegen.cpu.gen_cpp(new_res_with_ir)
-    # print(code)
+    test3()
